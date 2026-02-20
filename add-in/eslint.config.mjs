8d1f9f5f3c14ce650342bdb388d0ba883953eaf9
Zainkovskii <anton.zainkovskii@actionengine.com>
2026-02-20 11:49:42 +0100

    create frontend

---
+++ add-in/eslint.config.mjs
@@ -0,0 +1,45 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      eqeqeq: "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "dot-notation": "error",
+      "no-unused-vars": "error",
+      "import/order": "off",
+      "simple-import-sort/imports": "off",
+      "sort-imports": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+        alias: {
+          map: [["@", "./src"]],
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
---
+++ add-in/src/app/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@geotab/zenith";
+import "@geotab/zenith/dist/index.css";
+import "./style.css";
+
+const App = () => {
+  return (
+    <>
+      App
+      <Button>Button</Button>
+    </>
+  );
+};
+
+export default App;
