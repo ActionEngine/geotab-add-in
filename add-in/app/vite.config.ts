3279f08905c6a49097d67edad95050d2b9f96633
Viktor Belomestnov <belom88@yandex.ru>
2026-02-21 19:25:39 +0100

    fix(FE): build (#5)

--- add-in/app/vite.config.ts
+++ add-in/app/vite.config.ts
@@ -1,12 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: env.VITE_BASE_URL || "/",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
     },
-  },
+  };
 });
