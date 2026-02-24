d973d15d53ec60d8770e5387d41d4e7920ed2cc8
Viacheslav.Tsy <viacheslav.tsyplenkov@actionengine.com>
2026-02-24 09:44:46 +0100

    Add db case intensitive fields

--- backend/modules/geotab_database/models/geotab_database.py
+++ backend/modules/geotab_database/models/geotab_database.py
@@ -5,9 +5,10 @@ from database.database import Base
 
 class GeotabDatabase(Base):
     __tablename__ = "geotab_database"
+
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, index=True, nullable=False)
-    database_name = Column(String, nullable=False)
+    email = Column(String(collation="case_insensitive"), index=True, nullable=False)
+    database_name = Column(String(collation="case_insensitive"), nullable=False)
     credentials = Column(String, nullable=False)
     ingestion_status = Column(
         Enum(IngestionStatus, native_enum=False),
