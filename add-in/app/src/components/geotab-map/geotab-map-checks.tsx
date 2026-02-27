46b917d370d51dead918d0db9e28dff1177a68e4
Zainkovskii <anton.zainkovskii@actionengine.com>
2026-02-27 20:36:50 +0100

    fix (fe): heght second page

--- add-in/app/src/components/geotab-map/geotab-map-checks.tsx
+++ add-in/app/src/components/geotab-map/geotab-map-checks.tsx
@@ -289,7 +289,7 @@ const GeotabMapChecks = ({
   ]);
 
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div style={{ width: "100%", height: "100%" }}>
       <MapLibre
         ref={mapRef}
         initialViewState={{
