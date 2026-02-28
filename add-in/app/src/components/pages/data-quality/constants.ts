917079af8b4b4a1feaa3d9c2a84d74103d2bbbfc
Zainkovskii <anton.zainkovskii@actionengine.com>
2026-02-28 19:45:15 +0100

    feat (fe): add new checks

--- add-in/app/src/components/pages/data-quality/constants.ts
+++ add-in/app/src/components/pages/data-quality/constants.ts
@@ -4,7 +4,11 @@ export const validationTypeLabelMap: Record<ValidationType, string> = {
   [ValidationType.TELEPORTATION]: "Teleportation",
   [ValidationType.DISTANCE_TO_ROAD]: "Distance to road",
   [ValidationType.IDLE_OUTLIER]: "Idle outlier",
-  [ValidationType.ROAD_COUNTER_2H]: "Road counter 2h",
+  [ValidationType.ROAD_COUNTER_FUEL_CONSUMPTION]:
+    "Road Counter: Fuel Consumption",
+  [ValidationType.ROAD_COUNTER_COOLANT_TEMP]: "Road Counter: Coolant Temp",
+  [ValidationType.ROAD_COUNTER_EV_BATTERY_DISCHARGE]:
+    "Road Counter: EV Battery Discharge",
 };
 
 export const validationTypeTooltipMap: Record<ValidationType, string> = {
@@ -14,5 +18,9 @@ export const validationTypeTooltipMap: Record<ValidationType, string> = {
     "This metric evaluates the positioning accuracy of devices by analyzing the variance between the reported GPS coordinates and known road networks.",
   [ValidationType.IDLE_OUTLIER]:
     "This metric identifies vehicles that report excessive or unusual stationary durations.",
-  [ValidationType.ROAD_COUNTER_2H]: "Road counter 2h",
+  [ValidationType.ROAD_COUNTER_FUEL_CONSUMPTION]:
+    "Road Counter: Fuel Consumption",
+  [ValidationType.ROAD_COUNTER_COOLANT_TEMP]: "Road Counter: Coolant Temp",
+  [ValidationType.ROAD_COUNTER_EV_BATTERY_DISCHARGE]:
+    "Road Counter: EV Battery Discharge",
 };
