eaa3f6d26277f23ecc0e0b540a1882212fb1570a
Viktor Belomestnov <belom88@yandex.ru>
2026-03-01 13:05:36 +0100

    fix(FE): new checks tooltips (#116)

--- add-in/app/src/components/pages/data-quality/constants.ts
+++ add-in/app/src/components/pages/data-quality/constants.ts
@@ -16,7 +16,10 @@ export const validationTypeTooltipMap: Record<ValidationType, string> = {
     "This metric evaluates the positioning accuracy of devices by analyzing the variance between the reported GPS coordinates and known road networks.",
   [ValidationType.IDLE_OUTLIER]:
     "This metric identifies vehicles that report excessive or unusual stationary durations.",
-  [ValidationType.ROAD_COUNTER_FUEL_CONSUMPTION]: "Fuel Consumption",
-  [ValidationType.ROAD_COUNTER_COOLANT_TEMP]: "Coolant Temp",
-  [ValidationType.ROAD_COUNTER_EV_BATTERY_DISCHARGE]: "EV Battery Discharge",
+  [ValidationType.ROAD_COUNTER_FUEL_CONSUMPTION]:
+    "This metric evaluates the fuel efficiency of a vehicle by comparing its consumption against the historical average of all vehicles traversing the same road segment.",
+  [ValidationType.ROAD_COUNTER_COOLANT_TEMP]:
+    "This metric monitors engine operating temperatures by comparing them to the average thermal profile of all vehicles on the current road segment.",
+  [ValidationType.ROAD_COUNTER_EV_BATTERY_DISCHARGE]:
+    "This metric measures the rate of energy depletion by benchmarking a vehicle’s discharge against the average energy output of all EVs on the same road segment.",
 };
